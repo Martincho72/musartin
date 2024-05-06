5bfc0e936e2de180fafff3494385344341f29510
Juan Martín Barrachina Alfonso <123059057+Martincho72@users.noreply.github.com>
2024-05-07 00:09:49 +0200

    Delete musartin directory

--- musartin/ConexionBBDD/SesionIniciada.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start(); // Iniciar o reanudar sesión
-    // Verificar si el usuario no ha iniciado sesión
-    if (!isset($_SESSION['usuario'])) {
-        // Redirigir a la página de inicio de sesión
-        header('Location: ./iniciarSesion.html');
-    }
-?>
